2e9969608e84d3ad3b5e4c59f6abe11f0a3b620c
tugang <tugang@digitalchina.com>
2023-02-06 17:45:37 +0800

    format

--- multi-tenant-server/src/main/java/com/ruoyi/framework/security/service/SysPermissionService.java
+++ multi-tenant-server/src/main/java/com/ruoyi/framework/security/service/SysPermissionService.java
@@ -12,12 +12,11 @@ import com.ruoyi.project.system.service.ISysRoleService;
 
 /**
  * 用户权限处理
- * 
+ *
  * @author ruoyi
  */
 @Component
-public class SysPermissionService
-{
+public class SysPermissionService {
     @Autowired
     private ISysRoleService roleService;
 
@@ -26,20 +25,16 @@ public class SysPermissionService
 
     /**
      * 获取角色数据权限
-     * 
+     *
      * @param user 用户信息
      * @return 角色权限信息
      */
-    public Set<String> getRolePermission(SysUser user)
-    {
+    public Set<String> getRolePermission(SysUser user) {
         Set<String> roles = new HashSet<>();
         // 管理员拥有所有权限
-        if (SecurityUtils.isComAdmin(user))
-        {
+        if (SecurityUtils.isComAdmin(user)) {
             roles.add("admin");
-        }
-        else
-        {
+        } else {
             roles.addAll(roleService.selectRolePermissionByUserId(user.getUserId()));
         }
         return roles;
@@ -47,20 +42,16 @@ public class SysPermissionService
 
     /**
      * 获取菜单数据权限
-     * 
+     *
      * @param user 用户信息
      * @return 菜单权限信息
      */
-    public Set<String> getMenuPermission(SysUser user)
-    {
+    public Set<String> getMenuPermission(SysUser user) {
         Set<String> perms = new HashSet<String>();
         // 管理员拥有所有权限
-        if (SecurityUtils.isComAdmin(user))
-        {
+        if (SecurityUtils.isComAdmin(user)) {
             perms.add("*:*:*");
-        }
-        else
-        {
+        } else {
             perms.addAll(menuService.selectMenuPermsByUserId(user.getUserId()));
         }
         return perms;
