c53f1625f5ce25012c2ca267eee1d44f50718839
Zhangldc <zhangldc@digitalchina.com>
2023-02-07 14:55:40 +0800

    依赖新增---MyBatis-plus

---
+++ multi-tenant-server/src/main/test/com/ruoyi/RunYiApplicationTest.java
@@ -0,0 +1,16 @@
+package com.ruoyi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @ClassName: RunYiApplicationTest
+ * @Description: TODO
+ * @Author: CodeDan
+ * @Date: 2023/2/7 11:51
+ * @Version: 1.0.0
+ **/
+@SpringBootTest
+public class RunYiApplicationTest {
+    
+
+}
