81abbc1c69b804e6a52eb4fb4a8aae996f15979c
Zhangldc <zhangldc@digitalchina.com>
2023-02-07 11:06:54 +0800

    功能新增---网络请求工具类

---
+++ multi-tenant-server/src/main/java/com/ruoyi/demo/util/ApiOperationUtil.java
@@ -0,0 +1,16 @@
+package com.ruoyi.demo.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName: ApiOperationUtil
+ * @Description: 调用第三方API工具类
+ * @Author: CodeDan
+ * @Date: 2023/2/7 10:30
+ * @Version: 1.0.0
+ **/
+@Component
+public class ApiOperationUtil {
+
+
+}
